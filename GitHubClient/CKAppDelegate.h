5c93d553b5784f65e220415b009d7634c57077c5
Richard Lichkus <rpl5018@gmail.com>
2014-07-14 10:22:18 -0700

    Initial Commit

---
+++ GitHubClient/CKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CKAppDelegate.h
+//  GitHubClient
+//
+//  Created by Richard Lichkus on 7/14/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubClient/CKViewController.h
@@ -0,0 +1,13 @@
+//
+//  CKViewController.h
+//  GitHubClient
+//
+//  Created by Richard Lichkus on 7/14/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKViewController : UIViewController
+
+@end
